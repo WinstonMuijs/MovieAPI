ed338f6636846c6736e9769260b129a0b55645d3
Winston Muijs <winstonhmuijs@gmail.com>
2022-08-21 10:49:51 +0200

    Updated the index.js to requirements.

--- index.js
+++ index.js
@@ -1,71 +1,231 @@
 const express = require('express'),
-morgan = require('morgan'),
-fs = require('fs'),
-path = require('path');
+bodyParser = require('body-parser'),
+uuid = require('uuid'),
+morgan = require('morgan')
 
 const app = express();
 
-const accesLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
-
-
-let top10ArthouseMovies = [
+let movies = [
     {
         title: 'The Fountain',
-        director: 'Darren Aronofsky',
+        description: 'As a modern-day scientist, Tommy is struggling with mortality, desperately searching for the medical breakthrough that will save the life of his cancer-stricken wife, Izzi.',
+        genre: {
+            name: 'Drama',
+            description: 'In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.'
+        },
+        director: {
+            name:'Darren Aronofsky',
+            bio: 'Darren Aronofsky was born February 12, 1969, in Brooklyn, New York. Growing up, Darren was always artistic: he loved classic movies and, as a teenager, he even spent time doing graffiti art. After high school, Darren went to Harvard University to study film (both live-action and animation).',
+            birthyear: ' February 12, 1969 in Brooklyn, New York City, New York, USA',
+            deathyear: ''
+        },
+        actors : ['Hugh Jackma','Rachel Weisz', 'Sean Patrick'],
+        year: 2006 ,
+        score: 7.2,
+        rating:'PG-13',
+        imageURL: 'https://www.imdb.com/title/tt0414993/mediaviewer/rm2671676416/?ref_=tt_ov_i',
+        featured: true,
         length: '1h 37m'
     },
     {
         title: 'The Eternal Sunshine of the Spotless Mind ',
-        director: 'Michel Gondry',
+        description:'When their relationship turns sour, a couple undergoes a medical procedure to have each other erased from their memories.',
+        genre: {
+            name: 'Drama',
+            description:'In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.'
+        },
+        director: {
+            name: 'Michel Gondry',
+            bio: 'He grew up in Versailles with a family who was very influenced by pop music. When he was young, Gondry wanted to be a painter or an inventor. In the 80s he entered in an art school in Paris where he could develop his graphic skills and where he also met friends with whom he created a pop-rock band called Oui-Oui.',
+            birthyear:'May 8, 1963 in Versailles, Seine-et-Oise [now Yvelines], France',
+            deathyear: ''
+        },
+        actors : ['Jim Carrey','Kate Winslet','Tom Wilkinson'],
+        year: 2004,
+        score:8.3,
+        ratng:'R',
+        imageURL: 'https://www.imdb.com/title/tt0338013/mediaviewer/rm2954530560/?ref_=tt_ov_i',
+        featured: true,
         length: '1h 48m'
     },
     {
         title: 'The Tree of Life',
-        director: 'Terrence Malick',
+        description:'',
+        genre: {
+            name:'',
+            description:''
+        },
+        director: {
+            name: 'Terrence Malick',
+            bio:'',
+            birthyear:'',
+            deathyear: ''
+        },
+        actors: [],
+        year:'',
+        score:'',
+        rating:'',
+        imageURL:'',
+        featured: true,
         length: '2h 19m'
     },
     {
         title: '2001: Space Odyssey',
-        director: 'Stanley Kubrick',
+        decription:'',
+        genre: {
+            name: '',
+            description:''
+        },
+        director: {
+            name: 'Stanley Kubrick',
+            bio:'',
+            birthyear:'',
+            deathyear:''
+        },
+        actors:[],
+        year:'',
+        score:'',
+        rating:'',
+        imageURL:'',
+        featured:true,
         length: '2h 29m'
     },
     {
         title: 'Where The Wild Things Are',
-        author: 'Spike Jonze',
+        description:'',
+        genre: {
+            name:'',
+            description:''
+        },
+        director: {
+            name: 'Spike Jonze',
+            bio:'',
+            birthyear:'',
+            deathyear:''
+        },
+        actors:[],
+        year:'',
+        score:'',
+        rating:'',
+        imageURL:'',
+        featured: true,
         length: '1h 41m'
     },
     {
         title: 'Black Swan',
-        director: 'Darren Aronofsky',
+        description:'',
+        genre: {
+            name:'',
+            description:''
+        },
+        director: {
+            name:'Darren Aronofsky',
+            bio:'',
+            birthyear:'',
+            deathyear:''
+        },
+        actors: [],
+        year:'',
+        score:'',
+        rating:'',
+        imageURL:'',
+        featured: true,
         length: '1h 48m'
     },
     {
         title: 'Blade Runner',
-        director: 'Ridley Scott',
+        description:'',
+        genre: {
+            name:'',
+            description:''
+        },
+        director: { 
+            name:'Ridley Scott',
+            bio:'',
+            birthyear:'',
+            deathyear:''
+        },
+        actors: [],
+        year:'',
+        score:'',
+        rating:'',
+        imageURL:'',
+        featured:true,
         length: '1h 57m'
     },
     {
         title: 'Mulholland Drive',
-        director: 'David Lynch',
+        description:'',
+        genre: {
+            name:'',
+            description:''
+        },
+        director: {
+            name:'David Lynch',
+            bio:'',
+            birthyear:'',
+            deathyear:''
+        },
+        actors:[],
+        year:'',
+        score:'',
+        rating:'',
+        imageURL:'',
+        featured:true,
         length: '2h 27m'
     },
     {
         title: 'Moon',
-        director: 'Duncan Jones',
+        description:'',
+        genre:{
+            name:'',
+            description:''
+        },
+        director: {
+            name:'Duncan Jones',
+            bio:'',
+            birthyear:'',
+            deathyear:''
+        },
+        actors:[],
+        year:'',
+        score:'',
+        rating:'',
+        imageURL:'',
+        featured:true,
         length: '1h 37m'
     },
     {
         title: 'The Piano',
-        author: 'Jane Campion',
+        description:'',
+        genre:{
+            name:'',
+            description:''
+        },
+        director: {
+            name:'Jane Campion',
+            bio:'',
+            birthyear:'',
+            deathyear:''
+        },
+        actors:[],
+        year:'',
+        score:'',
+        rating:'',
+        imageURL:'',
+        featured: true,
         length: '2h 01m'
     }
 ];
 
-//Setup the logger
-app.use(morgan('combined', {stream: accesLogStream}));
+//Setup the logger to terminal
+app.use(morgan('common'));
 
 //Setup express.static for documentation
 app.use(express.static('public'));
+
+//body parser
+app.use(bodyParser.json());
 
 //GET requests
 app.get('/', (req, res)=>{
@@ -73,7 +233,26 @@ app.get('/', (req, res)=>{
 });
 
 app.get('/movies', (req, res)=> {
-    res.json(top10ArthouseMovies);
+    res.json(movies);
+});
+
+app.get('/movies/:title', (req, res) => {
+    res.json(movies.find ( (movie) =>
+    {return movie.title === req.params.title}));
+});
+
+app.post('/movies', (req, res) => {
+    let newMovie = req.body;
+
+    if(!newMovie.name) {
+        const message = 'Missing name in request body';
+        res.status(400).send(message);
+    }else {
+        newMovie.id = uuid.v4();
+        movies.push(newMovie);
+        res.status(201).send(newMovie);
+    }
+
 });
 
 //Error Handling 
