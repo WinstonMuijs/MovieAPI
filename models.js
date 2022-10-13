d2737279978673db3e3da06f30a7ac596544bbcd
Winston Muijs <winstonhmuijs@gmail.com>
2022-10-13 18:59:26 +0200

    update

--- models.js
+++ models.js
@@ -12,6 +12,7 @@ let movieSchema = mongoose.Schema({
 });
 
 let userSchema = mongoose.Schema({
+    _id : Number,
     name: {type: String, required: true},
     password: {type:String, required: true},
     email: {type: String, required: true},
