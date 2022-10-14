0b2a1bcdc0367187ec5609c0d8300084730fe885
Winston Muijs <winstonhmuijs@gmail.com>
2022-10-14 12:10:56 +0200

    Updated the models

--- models.js
+++ models.js
@@ -2,17 +2,23 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
-    _id : String,
     title: {type: String, required: true},
     description: {type: String, required: true},
-    genre: {type: mongoose.Schema.Types.ObjectId, ref: 'Genre'},
-    director: {type: mongoose.Schema.Types.ObjectId, ref: 'Director'},
+    genre: {
+        name: String,
+        description: String
+    },
+    director: {
+        name: String,
+        bio: String,
+        birthyear: String,
+        deathyear: String
+    },
     imageURL: String,
     featured: Boolean
 });
 
 let userSchema = mongoose.Schema({
-    _id : String,
     name: {type: String, required: true},
     password: {type:String, required: true},
     email: {type: String, required: true},
@@ -29,27 +35,9 @@ userSchema.methods.validatePassword = function(password) {
 };
 
 
-
-let genreSchema = mongoose.Schema({
-    _id : String,
-    name: {type: String, required: true},
-    description: {type: String, required: true}
-});
-
-let directorSchema = mongoose.Schema({
-    _id : String,
-    name: {type: String, required: true},
-    bio: {type:String, required: true},
-    birthday: Date,
-    deathyear: Date
-});
-
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User',userSchema);
-let Genre = mongoose.model('Genre',genreSchema);
-let Director = mongoose.model('Director', directorSchema);
+
 
 module.exports.Movie = Movie;
 module.exports.User = User;
-module.exports.Genre = Genre;
-module.exports.Director = Director;
